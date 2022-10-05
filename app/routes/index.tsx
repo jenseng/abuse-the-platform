145bef81ea336fd0e5874e405d60171dd3c6ddf3
Jon Jensen <jenseng@gmail.com>
2022-10-04 18:22:01 -0600

    basic keyboard

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,62 @@
+import styles from "~/styles/app.css";
+import * as Tone from "tone";
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
+
 export default function Index() {
+  return <Piano />;
+}
+
+function Piano() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="piano">
+      {Array(88)
+        .fill(null)
+        .map((_, i) => i)
+        .filter((i) => i >= 24 && i < 64)
+        .map((i) => (
+          <Key key={i} value={i} />
+        ))}
     </div>
   );
+}
+
+const Notes = [
+  "a",
+  "a#",
+  "b",
+  "c",
+  "c#",
+  "d",
+  "d#",
+  "e",
+  "f",
+  "f#",
+  "g",
+  "g#",
+] as const;
+
+type KeyProps = { value: number };
+function Key({ value }: KeyProps) {
+  const noteName = Notes[value % 12];
+  return (
+    <button
+      className={[
+        "key",
+        `key--${noteName.replace("#", "sharp")}`,
+        noteName.endsWith("#") && "key--ebony",
+      ]
+        .filter(Boolean)
+        .join(" ")}
+      onClick={() => play(noteName, Math.floor((value + 9) / 12))}
+    />
+  );
+}
+
+const synth: Tone.Synth =
+  typeof window != "undefined" ? new Tone.Synth().toDestination() : ({} as any);
+function play(noteName: string, octave: number) {
+  synth.triggerAttackRelease(`${noteName}${octave}`, "8n");
 }
